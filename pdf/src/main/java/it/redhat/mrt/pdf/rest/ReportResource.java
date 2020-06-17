a430e65f315994b4a4cc952031b19ac164878f47
gmagnotta <giuseppe.magnotta@gmail.com>
2020-06-17 13:18:32 +0200

    Implemented a logic to return a list of reports names given the rhid and the year. The list is always reverse sorted, so the latest report is the first element of the array.
    closes #7

--- pdf/src/main/java/it/redhat/mrt/pdf/rest/ReportResource.java
+++ pdf/src/main/java/it/redhat/mrt/pdf/rest/ReportResource.java
@@ -1,6 +1,12 @@
 package it.redhat.mrt.pdf.rest;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.regex.Matcher;
@@ -12,12 +18,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.io.FilenameUtils;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -79,7 +87,60 @@ public class ReportResource {
         return response.build();
 
     }
-    
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    /**
+     * Return a list of report names given the rhid and year
+     */
+    public List<String> getReportsByYear(@QueryParam("rhid") String rhid, @QueryParam("year") int year) {
+
+		if (rhid == null) {
+			throw new WebApplicationException(
+					Response.status(Response.Status.BAD_REQUEST).entity("rhid parameter is mandatory").build());
+		}
+
+		if (year == 0) {
+
+			year = Calendar.getInstance().get(Calendar.YEAR);
+
+		} else if (year <= 0 || year > Calendar.getInstance().get(Calendar.YEAR)) {
+
+			throw new WebApplicationException(
+					Response.status(Response.Status.BAD_REQUEST).entity("year must be valid").build());
+
+		}
+
+		final String startString = rhid + "_" + year;
+
+    	List<String> listFiles = new ArrayList<>();
+        File fileFolder = new File(PdfBuilder.reportDirectory);
+        File[] list = fileFolder.listFiles(new FilenameFilter() {
+
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.startsWith(startString) ? true : false;
+			}
+
+		});
+
+        // Return a reverse ordered array
+        Arrays.sort(list, (a, b) -> -a.getName().compareTo(b.getName()));
+
+        for (File f: list) {
+
+        	if (!f.isDirectory()) {
+
+                listFiles.add(FilenameUtils.removeExtension(f.getName()));
+
+            }
+
+        }
+
+        return listFiles;
+
+    }
+
     @POST
     @Path("/build/{rhid}/{year}/{month}")
     public String build(@PathParam("rhid") String rhid,@PathParam("year") int year,@PathParam("month") int month) {
