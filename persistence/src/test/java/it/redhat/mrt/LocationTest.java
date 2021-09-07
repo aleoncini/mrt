858eee65b3a5ec187109dfba20a7a1294fe3ebcc
Andrea Leoncini <andrea.leoncini@gmail.com>
2021-09-07 16:10:53 +0200

    deploy locally with podman

--- persistence/src/test/java/it/redhat/mrt/LocationTest.java
+++ persistence/src/test/java/it/redhat/mrt/LocationTest.java
@@ -4,18 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+public class LocationTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/locations")
           .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .statusCode(200);
     }
 
 }
