64b9a56179343a3c9d9faeab142977ae5e001ecc
Andrea Leoncini <andrea.leoncini@gmail.com>
2021-12-03 12:04:40 +0100

    ns

--- persistence/src/main/java/it/redhat/mrt/rest/KeyResource.java
+++ persistence/src/main/java/it/redhat/mrt/rest/KeyResource.java
@@ -8,12 +8,16 @@ import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import it.redhat.mrt.model.Key;
 
 @Path("/key")
 @Produces(MediaType.APPLICATION_JSON)
 public class KeyResource {
+
+    private static final Logger logger = LoggerFactory.getLogger("it.redhat.mrt");
 
     @Inject
     JsonWebToken accessToken;
@@ -23,6 +27,7 @@ public class KeyResource {
 
     @GET
     public Key get() {
+        logger.info("==============> requested Google Maps API KEY");
         return new Key().setName("google.maps.api").setValue(keyValue);
     }
 
