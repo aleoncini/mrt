0ba5fa6faa5b8828792f4aeed7989d8a36aa5be0
Laiot <carmelo.sarta.main@gmail.com>
2022-01-10 11:00:42 +0100

    Started implementing Patternfly design

--- frontend-ang/src/app/app.module.ts
+++ frontend-ang/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -64,6 +64,7 @@ import {MatTableModule} from '@angular/material/table';
     MatTableModule
   ],
   providers: [MatDatepickerModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
