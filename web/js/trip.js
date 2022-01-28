f081db567a788f59f053346be80ac6686d47de89
Andrea Leoncini <andrea.leoncini@gmail.com>
2022-01-28 12:46:43 +0100

    new module WEB to serve static html pages

---
+++ web/js/trip.js
@@ -0,0 +1,148 @@
+var STORE_ORIGIN = window.location.origin;
+var the_destination = {};
+
+function initKey() {
+    var theUrl = STORE_ORIGIN + '/key';
+    $.ajax({
+        url: theUrl,
+        type: 'GET',
+        dataType: 'json',
+        complete: function(response, status, xhr){
+            var keyObject = jQuery.parseJSON(response.responseText);
+            console.log('Key READY...');
+            initGoogleScript(keyObject.value);
+        }
+    });
+};
+
+function initGoogleScript(theKey) {
+    var the_script = document.createElement( 'script' );
+    var the_src = 'https://maps.googleapis.com/maps/api/js?key=' + theKey + '&callback=initMap&libraries=places';
+    the_script.setAttribute( 'src', the_src );
+    the_script.setAttribute('defer', null);
+    the_script.setAttribute('async', null);
+    console.log('Appending the script...');
+    document.body.appendChild( the_script );
+};
+
+function initMap() {
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+
+    const componentForm = [
+        'location',
+    ];
+ 
+    const rome_office_position = {lat: 41.909832599799316, lng: 12.452532095955236};
+
+    const options = {zoom: 13, zoomControl: true, mapTypeControl: false, streetViewControl: false, scaleControl: true, center: rome_office_position};
+    map = new google.maps.Map(document.getElementById('map'), options);
+
+    const image = "/images/rh_flag.png";
+    const rome_office_marker = new google.maps.Marker({
+        map: map,
+        title: "Red Hat sede di Roma",
+        position: rome_office_position,
+        icon: image,
+        draggable: false
+    });
+    const marker = new google.maps.Marker({map: map, draggable: false});
+    marker.setVisible(false);
+
+    directionsRenderer.setMap(map);  
+    const onChangeHandler = function () {
+        directionsService.route(
+            {
+                origin: rome_office_position,
+                destination: the_destination,
+                travelMode: google.maps.TravelMode.DRIVING
+            },
+            (response, status) => {
+                calculateRouteDistance(response.routes[0].legs[0].distance.value);
+                console.log(status);
+                directionsRenderer.setDirections(response);
+            }
+        )
+    };
+    document.getElementById("checkOutButton").addEventListener("click", onChangeHandler);
+
+    const autocompleteInput = document.getElementById('location');
+    const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+      fields: ["address_components", "geometry", "name"],
+      types: ["address"],
+    });
+    autocomplete.addListener('place_changed', function () {
+        const place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert('No details available for input: \'' + place.name + '\'');
+            return;
+        }
+        $('#distance').html('');
+        renderAddress(place);
+        fillInAddress(place);
+    });
+
+    function fillInAddress(place) {  // optional parameter
+        const addressNameFormat = {
+            'street_number': 'short_name',
+            'route': 'long_name',
+            'locality': 'long_name',
+            'administrative_area_level_1': 'short_name',
+            'country': 'long_name',
+            'postal_code': 'short_name',
+        };
+        const getAddressComp = function (type) {
+            for (const component of place.address_components) {
+                if (component.types[0] === type) {
+                    return component[addressNameFormat[type]];
+                }
+            }
+            return '';
+        };
+        document.getElementById('location').value = getAddressComp('street_number') + ' ' + getAddressComp('route');
+    };
+
+    function renderAddress(place) {
+      map.setCenter(place.geometry.location);
+      the_destination = place.geometry.location;
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+    };
+};
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    directionsService.route(
+        {
+            origin: {lat: 41.909832599799316, lng: 12.452532095955236},
+            destination: {lat: 41.831200940073444, lng: 12.467770885340915},
+            travelMode: google.maps.TravelMode.DRIVING
+        },
+        (response, status) => {
+            console.log(response);
+            console.log(status);
+            directionsRenderer.setDirections(response);
+        }
+    )
+};
+
+function calculateRouteDistance(routeDistance) {
+    const totalDistance = Math.ceil((routeDistance * 2.4) / 1000);
+    $('#distance').html(totalDistance);
+};
+
+function saveTrip(the_trip, callbackFunction) {
+    var theUrl = STORE_ORIGIN + '/trips';
+    $.ajax({
+        type: "POST",
+        url: theUrl,
+        data: JSON.stringify(the_trip),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        complete: function(response, status, xhr){
+            var trip = jQuery.parseJSON(response.responseText);
+            callbackFunction(trip);
+        }
+    });
+};
---
+++ web/js/trips.js
@@ -0,0 +1,148 @@
+var STORE_ORIGIN = window.location.origin;
+
+
+function formatLocationList(locations) {
+    $('#tList').empty();
+    var options = "";
+    $.each(locations, function (index, location) {
+        options += "<option value='" + location.destination + "'>" + location.distance + "</option>";
+    });
+    $('#tList').append(options);
+};
+
+function displayAllTrips(trips) {
+    $('#tripsTableContainer').hide();
+    $("#tripsTableBody").empty();
+    $('#bannerNumberOfTrips').text('0');
+    $('#bannerTotalMileage').text('0');
+    $.each(trips, function (index, trip) {
+        addTripToTable(trip)
+    });
+    $('#tripsBannerContainer').show();
+    $('#tripsTableContainer').show(500);
+};
+
+function addTripToTable(trip) {
+    var numberOfTrips = parseInt($('#bannerNumberOfTrips').text());
+    var totalMileage = parseInt($('#bannerTotalMileage').text(), 10);
+    var rowContent = '<tr>';
+    rowContent += '<td>' + trip.day + '</td>';
+    rowContent += '<td>' + trip.destination + '</td>';
+    rowContent += '<td>' + trip.purpose + '</td>';
+    rowContent += '<td>' + trip.distance + '</td>';
+    rowContent += '<td style="cursor: pointer;" class="delete_trip" data-id="' + trip.id + '"><img src="images/trash.svg" alt="delete" width="24" height="24"></td>';
+    rowContent += '</tr>';
+    $('#tbl_trips  tbody').append(rowContent);
+    numberOfTrips += 1;
+    totalMileage += trip.distance;
+    $('#bannerNumberOfTrips').text(numberOfTrips);
+    $('#bannerTotalMileage').text(totalMileage);
+};
+
+function showReportBuildResult(answer) {
+    $("#reportSuccessfullyRequested").fadeIn();
+    window.setTimeout(function () { 
+        $("#reportSuccessfullyRequested").fadeOut();
+    }, 2000);
+};
+
+function displayReportList(reportList) {
+    $("#reportsTableBody").empty();
+    $.each(reportList, function (index, reportInfo) {
+        addReportInfoToTable(reportInfo);
+    });
+};
+
+function addReportInfoToTable(info) {
+    var rowContent = '<tr>';
+    rowContent += '<td>' + info.month + '</td>';
+    rowContent += '<td>' + info.creationTime + '</td>';
+    rowContent += '<td>' + info.size + '</td>';
+    rowContent += '<td>' + info.version + '</td>';
+    rowContent += '<td><a href="/archive/pdf/' + info.name + '"><img src="images/file-earmark-arrow-down.svg" alt="download" width="32" height="32"></a></td>';
+    rowContent += '<td style="cursor: pointer;" class="delete_report" data-filename="' + info.name + '"><img src="images/trash.svg" alt="delete" width="32" height="32"></td>';
+    rowContent += '</tr>';
+    $('#tbl_reports  tbody').append(rowContent);
+};
+
+// --- STORAGE FUNCTIONS ----------------------------------------- 
+function saveTrip(the_trip, callbackFunction) {
+    var theUrl = STORE_ORIGIN + '/trips';
+    $.ajax({
+        type: "POST",
+        url: theUrl,
+        data: JSON.stringify(the_trip),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        complete: function(response, status, xhr){
+            var trip = jQuery.parseJSON(response.responseText);
+            callbackFunction(trip);
+        }
+    });
+};
+
+function deleteTrip(id) {
+    var theUrl = STORE_ORIGIN + '/trips/' + id;
+    $.ajax({
+        url: theUrl,
+        type: 'DELETE',
+        dataType: 'json',
+        complete: function(response, status, xhr){
+            console.log('TRIP ' + id + ' removed from DB!');
+        }
+    });
+};
+
+function loadTrips(year, month, callbackFunction) {
+    var theUrl = STORE_ORIGIN + '/trips/' + year + "/" + month;
+    $.ajax({
+        url: theUrl,
+        type: 'GET',
+        dataType: 'json',
+        complete: function(response, status, xhr){
+            var trips = jQuery.parseJSON(response.responseText);
+            callbackFunction(trips);
+        }
+    });
+};
+
+function requestReportBuild(month, callbackFunction) {
+    var year = new Date().getFullYear();
+    var theUrl = STORE_ORIGIN + '/reports/' + year + '/' + month;
+    $.ajax({
+        url: theUrl,
+        type: 'POST',
+        dataType: 'json',
+        complete: function(response, status, xhr){
+            var answer = jQuery.parseJSON(response.responseText);
+            callbackFunction(answer);
+        }
+    });
+};
+
+// --- PDF ARCHIVE FUNCTIONS ----------------------------------------- 
+
+function loadReportList(year, callbackFunction) {
+    var theUrl = STORE_ORIGIN + '/archive/' + year;
+    $.ajax({
+        url: theUrl,
+        type: 'GET',
+        dataType: 'json',
+        complete: function(response, status, xhr){
+            var data = jQuery.parseJSON(response.responseText);
+            callbackFunction(data);
+        }
+    });
+};
+
+function deleteReportFile(filename) {
+    var theUrl = STORE_ORIGIN + '/archive/' + filename;
+    $.ajax({
+        url: theUrl,
+        type: 'DELETE',
+        dataType: 'json',
+        complete: function(response, status, xhr){
+            console.log('FILE ' + filename + ' removed from file system!');
+        }
+    });
+};
