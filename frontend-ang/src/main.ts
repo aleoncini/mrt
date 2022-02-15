1d7222a009d41d64567771d33f2382b89f9f8d6e
Andrea Leoncini <andrea.leoncini@gmail.com>
2022-02-15 14:25:28 +0100

    added keycloak security to angular app

--- frontend-ang/src/main.ts
+++ frontend-ang/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Keycloak from 'keycloak-js';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +9,14 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+
+//keycloak init options
+let initConfig = {
+  url: 'https://access-sso.apps.ocp4.rhocplab.com/auth/',
+  realm: 'rh',
+  clientId: 'mrt'
+}
+
+let keycloak = Keycloak(initConfig);
+
+keycloak.init({ onLoad: "login-required" });
