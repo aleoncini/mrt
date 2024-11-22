9cdc8233cc7c1863737ff298f80a70f99740d40a
Andrea Leoncini <andrea.leoncini@gmail.com>
2024-11-22 14:08:24 +0100

    some fix to interface

--- docs/js/mrt.js
+++ docs/js/mrt.js
@@ -3,27 +3,10 @@ var STORE_ORIGIN = window.location.origin;
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
 
-function checkReport() {
-    // Retrieving report from local storage
-    obj = localStorage.getItem('mrtReport');
-    if( obj != null ){
-                    console.log("======> CR REPORT: " + report);
-                    console.log("======> CR  TRIPS: " + report.trips.length);
-        report = obj;
-                    console.log("======> CR REPORT: " + report);
-                    console.log("======> CR  TRIPS: " + report.trips.length);
-    }
-    configReportForm();
-
-    $('#current_report').show(500);
-};
-
 function configReportForm() {
     if(report.latestOdometer == null || report.latestOdometer == 0){
         report.latestOdometer = 0;
     }
-                console.log("======> ODO: " + report.latestOdometer);
-
     $('#odoStartInput').val(report.latestOdometer + calculateOdometerStart());
 };
 
@@ -40,7 +23,9 @@ function setAlert(msg) {
 };
 
 function resetReport() {
-    localStorage.removeItem("mrtReport");
+    var trips = [];
+    localStorage.setItem('mrtTrips', JSON.stringify(trips));
+    displayAllTrips();
 };
 
 function displayAllTrips() {
@@ -64,10 +49,19 @@ function addTripToTable(trip) {
     rowContent += '<td>' + trip.odometerStart + '</td>';
     rowContent += '<td>' + trip.from + '</td>';
     rowContent += '<td>' + trip.destination + '</td>';
+    rowContent += '<td><img src="img/';
+    if(trip.twoWays){
+        rowContent += 'check-';
+    }
+    rowContent += 'circle.svg" width="24" height="24"></td>';
     rowContent += '<td>' + trip.purpose + '</td>';
     rowContent += '<td>' + trip.odometerEnd + '</td>';
     rowContent += '<td>' + trip.distance + '</td>';
     rowContent += '<td style="cursor: pointer;" class="delete_trip"><img src="img/trash.svg" alt="delete" width="24" height="24"></td>';
     rowContent += '</tr>';
     $('#tbl_trips  tbody').append(rowContent);
+};
+
+function setOdometerStart() {
+    console.log("=======================");
 };
